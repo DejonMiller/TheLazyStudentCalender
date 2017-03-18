29ae11e1c9e2e6d8c5c33b5f37cb9ce0a93b9333
DejonMiller <DejonMiller93@gmail.com>
2017-03-18 03:18:52 -0400

    first commit

---
+++ app/src/main/java/ocr/swelsc/autoImport.java
@@ -0,0 +1,21 @@
+package ocr.swelsc;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class autoImport extends Fragment {
+
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.autoimport,container,false);
+        return view;
+    }
+}
---
+++ app/src/main/java/ocr/swelsc/manualImport.java
@@ -0,0 +1,21 @@
+package ocr.swelsc;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class manualImport extends Fragment {
+
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.manualimport, container, false);
+        return view;
+    }
+}
